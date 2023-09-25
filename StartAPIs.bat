1a42cd7c91a1c42b4391221abbbbf4518ca41939
Moacyr <99156792+Moriblo@users.noreply.github.com>
2023-09-25 18:50:06 -0300

    Update StartAPIs.bat

--- StartAPIs.bat
+++ StartAPIs.bat
@@ -4,7 +4,7 @@ https://drive.google.com/file/d/16TJzEJE9miSXwyhG-2ns1cXjXy-1iMUZ/view?usp=drive
 https://drive.google.com/file/d/1_lEP_zATl03vcdekok_sV7MqMTY1gq1K/view?usp=drive_link
 https://drive.google.com/file/d/15XkBvyo_p3KqqoOq6AgHxrtTukK4-IK9/view?usp=drive_link
 
-REM Executar os arquivo .exe para cada API
+REM Executa os arquivos .exe para cada API
 title app
 start app.exe
 
