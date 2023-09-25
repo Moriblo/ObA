3681e9844904f57477a2f81f9311a073c3b3da45
Moacyr <99156792+Moriblo@users.noreply.github.com>
2023-09-25 18:49:23 -0300

    Add files via upload

---
+++ StartAPIs.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Baixa as APIs de um diretório APIs do google drive
+https://drive.google.com/file/d/16TJzEJE9miSXwyhG-2ns1cXjXy-1iMUZ/view?usp=drive_link
+https://drive.google.com/file/d/1_lEP_zATl03vcdekok_sV7MqMTY1gq1K/view?usp=drive_link
+https://drive.google.com/file/d/15XkBvyo_p3KqqoOq6AgHxrtTukK4-IK9/view?usp=drive_link
+
+REM Executar os arquivo .exe para cada API
+title app
+start app.exe
+
+title tradutor
+start tradutor.exe
+
+title smuseum
+start smuseum.exe
